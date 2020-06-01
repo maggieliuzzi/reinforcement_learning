480094f4f52efb540a0e6d7fb1e0ce107b48c262
Maggie Liuzzi <11723321+maggieliuzzi@users.noreply.github.com>
2020-06-01 17:15:14 +1000

    Created utils.py for GridWorld

---
+++ environments/utils.py
@@ -0,0 +1,25 @@
+'''
+Useful functions to print value and policy for the GridWorld environment.
+'''
+
+
+def print_values(V, g):
+  for i in range(g.rows):
+    print("---------------------------")
+    for j in range(g.cols):
+      v = V.get((i,j), 0)
+      if v >= 0:
+        print(" %.2f|" % v, end="")
+      else:
+        print("%.2f|" % v, end="") # -ve sign takes up an extra space
+    print("")
+
+
+def print_policy(P, g):
+  for i in range(g.rows):
+    print("---------------------------")
+    for j in range(g.cols):
+      a = P.get((i,j), ' ')
+      print("  %s  |" % a, end="")
+    print("")
+ 
